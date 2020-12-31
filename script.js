0dec05930cf959f4ec0ba86e26405aae1808e538
ParkEunchong(Chris) <eunpark@bizflow.com>
2020-12-31 20:04:11 +0900

    change

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ const image2 = document.getElementById("image2")
 const image3 = document.getElementById("image3")
 const textBox = document.querySelectorAll(".text-box")
 // Dark or Light Images
-function imageMode(color) {
-    image1.src = `img/undraw_proud_coder_${color}.svg`;
-    image2.src = `img/undraw_feeling_proud_${color}.svg`;
-    image3.src = `img/undraw_conceptual_idea_${color}.svg`;
-}
+// function imageMode(color) {
+//     image1.src = `img/undraw_proud_coder_${color}.svg`;
+//     image2.src = `img/undraw_feeling_proud_${color}.svg`;
+//     image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+// }
 
 // Dark Light Mode styles
 
@@ -23,7 +23,7 @@ function toggleDarkLightMode(mode) {
     toggleIcon.children[0].textContent = mode === "dark"  ? 'Dark Mode' : 'Light Mode';
     mode === "dark" ? toggleIcon.children[1].classList.replace("fa-sun", "fa-moon")
     : toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
-    mode === "dark" ? imageMode("dark") : mageMode("light")
+    // mode === "dark" ? imageMode("dark") : mageMode("light")
 }
     
 
@@ -45,14 +45,14 @@ toggleSwitch.addEventListener("change", switchTheme);
 
 // Check Local Storage for Theme
 const currentTheme = localStorage.getItem("theme");
-console.log(currentTheme)
-if (currentTheme) {
-    document.documentElement.setAttribute("data-theme", currentTheme);
-    if (currentTheme === "dark") {
-        toggleSwitch.checked = true;
-        darkMode();
-    } 
-}
+// console.log(currentTheme)
+// if (currentTheme) {
+//     document.documentElement.setAttribute("data-theme", currentTheme);
+//     if (currentTheme === "dark") {
+//         toggleSwitch.checked = true;
+//         darkMode();
+//     } 
+// }
 
 
 
